cf8411400bf6df5916db2d9280879fc5c4bd1f28
Peace Akib <99202770+pe-a-ce@users.noreply.github.com>
2022-05-18 20:34:26 +0100

    Surgery class completed

--- HospitalSurgery/src/main/java/com/example/HospitalSurgery/model/Surgery.java
+++ HospitalSurgery/src/main/java/com/example/HospitalSurgery/model/Surgery.java
@@ -1,4 +1,51 @@
 package com.example.HospitalSurgery.model;
 
+
+import javax.persistence.*;
+
+@Entity
 public class Surgery {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Enumerated
+    @Column(name="Department")
+    private SurgeryEnum Department;
+
+    // no arg constructor
+    public Surgery() {}
+    // normal constructor
+    public Surgery(Long id, String name, SurgeryEnum department) {
+        this.id = id;
+        this.name = name;
+        Department = department;
+    }
+
+        // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public SurgeryEnum getDepartment() {
+        return Department;
+    }
+
+    public void setDepartment(SurgeryEnum department) {
+        Department = department;
+    }
 }
---
+++ HospitalSurgery/src/main/java/com/example/HospitalSurgery/model/SurgeryEnum.java
@@ -0,0 +1,16 @@
+package com.example.HospitalSurgery.model;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+
+public enum SurgeryEnum {
+
+    ORTHOPEDICS,
+    PLASTICS,
+    NEUROLOGY,
+    TRAUMA,
+    GENERAL,
+    PAEDIATRIC
+
+}
